f27b02e967be76cc0d3b5b014eb5ecc6e4903605
Piyush Kumar <piyush31221@gmail.com>
2025-08-11 01:57:53 +0530

    updated routes listing.js

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User=require("../models/user.js");
-const wrapAsync = require("../Utils/wrapAsync");
+const wrapAsync = require("../utils/wrapAsync");
 const passport=require("passport");
 const { saveRedirectUrl } = require("../middleware.js");
 const userController=require("../controllers/users.js");
